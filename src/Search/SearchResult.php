5d6b5ce50b42529f7706dfb3b5d822dfc3310755
uikolas <uikolas@gmail.com>
2018-03-14 21:00:52 +0200

    Added searcher test

--- src/Search/SearchResult.php
+++ src/Search/SearchResult.php
@@ -44,11 +44,7 @@ class SearchResult implements \Countable, \JsonSerializable
     }
 
     /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
+     * @return array
      */
     public function jsonSerialize()
     {
